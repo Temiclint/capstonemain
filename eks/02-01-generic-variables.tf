98d5c364d1800adbe50432305938ee1358b31373
temiclint <direadelaja@gmail.com>
2024-03-15 21:03:00 +0100

    capstone project code

---
+++ eks/02-01-generic-variables.tf
@@ -0,0 +1,19 @@
+# Input Variables
+
+# AWS Region
+variable "aws_region" {
+  description = "AWS Resource region"
+  type        = string
+}
+
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable"
+  type        = string
+}
+
+# Business Division
+variable "business_division" {
+  description = "Business Division this Infrastructure belongs"
+  type        = string
+}
