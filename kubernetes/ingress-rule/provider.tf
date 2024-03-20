954111a00ff01adc1b5ee3833670183c4091ad4f
temiclint <direadelaja@gmail.com>
2024-03-20 19:07:11 +0100

    fixes

--- kubernetes/ingress-rule/provider.tf
+++ kubernetes/ingress-rule/provider.tf
@@ -18,11 +18,11 @@ terraform {
 }
 
 
-data "aws_eks_cluster" "hr-dev-eks-demo" {
-  name = "hr-dev-eks-demo"
+data "aws_eks_cluster" "student-dev-eks-capstone1" {
+  name = "student-dev-eks-capstone1"
 }
-data "aws_eks_cluster_auth" "hr-dev-eks-demo_auth" {
-  name = "hr-dev-eks-demo_auth"
+data "aws_eks_cluster_auth" "student-dev-eks-capstone1_auth" {
+  name = "student-dev-eks-capstone1_auth"
 }
 
 
@@ -32,17 +32,17 @@ provider "aws" {
 
 provider "helm" {
     kubernetes {
-       #host                   = data.aws_eks_cluster.hr-dev-eks-demo.endpoint
-      # cluster_ca_certificate = base64decode(data.aws_eks_cluster.hr-dev-eks-demo.certificate_authority[0].data)
-       #token                  = data.aws_eks_cluster_auth.hr-dev-eks-demo_auth.token
+       #host                   = data.aws_eks_cluster.student-dev-eks-capstone1.endpoint
+      # cluster_ca_certificate = base64decode(data.aws_eks_cluster.student-dev-eks-capstone1.certificate_authority[0].data)
+       #token                  = data.aws_eks_cluster_auth.student-dev-eks-capstone1_auth.token
        config_path = "~/.kube/config"
     }
 }
 
 provider "kubernetes" {
-  #host                   = data.aws_eks_cluster.hr-dev-eks-demo.endpoint
- # cluster_ca_certificate = base64decode(data.aws_eks_cluster.hr-dev-eks-demo.certificate_authority[0].data)
-  #token                  = data.aws_eks_cluster_auth.hr-dev-eks-demo_auth.token
+  #host                   = data.aws_eks_cluster.student-dev-eks-capstone1.endpoint
+ # cluster_ca_certificate = base64decode(data.aws_eks_cluster.student-dev-eks-capstone1.certificate_authority[0].data)
+  #token                  = data.aws_eks_cluster_auth.student-dev-eks-capstone1_auth.token
  #  version          = "2.16.1"
   config_path = "~/.kube/config"
 }
@@ -50,8 +50,8 @@ provider "kubernetes" {
 provider "kubectl" {
    load_config_file = false
    alias = "aws"
-   host                   = data.aws_eks_cluster.hr-dev-eks-demo.endpoint
-   cluster_ca_certificate = base64decode(data.aws_eks_cluster.hr-dev-eks-demo.certificate_authority[0].data)
-   token                  = data.aws_eks_cluster_auth.hr-dev-eks-demo_auth.token
+   host                   = data.aws_eks_cluster.student-dev-eks-capstone1.endpoint
+   cluster_ca_certificate = base64decode(data.aws_eks_cluster.student-dev-eks-capstone1.certificate_authority[0].data)
+   token                  = data.aws_eks_cluster_auth.student-dev-eks-capstone1_auth.token
    config_path = "~/.kube/config"
 }
